07dd8feab3f0d10ffa1d261cfe89f83cf51581b7
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2019-09-10 09:52:38 +0300

    Add calculator (not finished).

--- Hello_World/Hello_World/SecondActivity.cs
+++ Hello_World/Hello_World/SecondActivity.cs
@@ -15,11 +15,34 @@ namespace Hello_World
     [Activity(Label = "Second_Activity")]
     public class SecondActivity : Activity
     {
+        //Button button;...
+        EditText _lisa_arv_1;
+        EditText _lisa_arv_2;
+        Button _liida_tehe;
+        Button _lahuta_tehe;
+        Button _korruta_tehe;
+        Button _jaga_tehe;
+        TextView _vastus_1;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.second_layout);
-            // Create your application here
+
+            _lisa_arv_1= FindViewById<EditText>(Resource.Id.lisa_arv_1);
+            _lisa_arv_2 = FindViewById<EditText>(Resource.Id.lisa_arv_2);
+            _liida_tehe = FindViewById<Button>(Resource.Id.liida_tehe);
+            _lahuta_tehe = FindViewById<Button>(Resource.Id.lahuta_tehe);
+            _korruta_tehe = FindViewById<Button>(Resource.Id.korruta_tehe);
+            _jaga_tehe = FindViewById<Button>(Resource.Id.jaga_tehe);
+            _vastus_1 = FindViewById<TextView>(Resource.Id.vastus_1);
+
+            //_button.Click += Button_Click;
+
+            //private void Button_Click(object sender, EventArgs e)
+            //{
+
+            //}
         }
     }
 }
