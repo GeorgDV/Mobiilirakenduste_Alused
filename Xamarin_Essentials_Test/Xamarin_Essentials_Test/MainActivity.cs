97b9ecbd215f7241dcbaba25df1b21d0124f5064
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2019-09-19 09:51:18 +0300

    Add Xamarin.Essentials application, batteryLevel checker.

---
+++ Xamarin_Essentials_Test/Xamarin_Essentials_Test/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace Xamarin_Essentials_Test
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView _batteryLevelTextView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            var _batteryLevelTextView = FindViewById<TextView>(Resource.Id.batteryLevelTextView);
+            var batteryLevel = Battery.ChargeLevel;
+            _batteryLevelTextView.Text = batteryLevel.ToString();
+
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            _batteryLevelTextView.Text = Battery.ChargeLevel.ToString();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
