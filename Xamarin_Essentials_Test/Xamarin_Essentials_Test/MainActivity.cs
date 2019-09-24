19cc6a1c8eb1a9e298863945babe40a5c3f00eb0
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2019-09-24 08:48:57 +0300

    Add battery level shower and debug message.

--- Xamarin_Essentials_Test/Xamarin_Essentials_Test/MainActivity.cs
+++ Xamarin_Essentials_Test/Xamarin_Essentials_Test/MainActivity.cs
@@ -24,6 +24,17 @@ namespace Xamarin_Essentials_Test
             _batteryLevelTextView.Text = batteryLevel.ToString();
 
             Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+
+            var deviceMode1 = DeviceInfo.Model;
+            var manufacturer = DeviceInfo.Manufacturer;
+            var deviceName = DeviceInfo.Name;
+            var version = DeviceInfo.Version;
+            var platform = DeviceInfo.Platform;
+            var idiom = DeviceInfo.Idiom;
+            var deviceType = DeviceInfo.DeviceType;
+
+            System.Diagnostics.Debug.WriteLine("Deviceinfo: {0}, {1}, {2}, {3}, {4}, {5}, {6}",
+                deviceMode1, manufacturer, deviceName, version, platform, idiom, deviceType);
         }
 
         private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
