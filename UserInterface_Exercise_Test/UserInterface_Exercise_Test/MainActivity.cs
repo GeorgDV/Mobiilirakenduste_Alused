be7010c7b58823c05966fa839aafad51a12c26a4
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2019-10-08 09:22:14 +0300

    Add webclient to UserInterface_Exercise_Test.

--- UserInterface_Exercise_Test/UserInterface_Exercise_Test/MainActivity.cs
+++ UserInterface_Exercise_Test/UserInterface_Exercise_Test/MainActivity.cs
@@ -4,12 +4,15 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Android.Views;
+using Android.Webkit;
 
 namespace UserInterface_Exercise_Test
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        WebView _webView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -18,6 +21,21 @@ namespace UserInterface_Exercise_Test
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
             ActionBar.Title = "My Toolbar";
+
+            var _webView = FindViewById<WebView>(Resource.Id.webView1);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.SetWebViewClient(new HelloWebViewClient());
+            _webView.LoadUrl("https://www.google.com/");
+        }
+
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && _webView.CanGoBack())
+            {
+                _webView.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
