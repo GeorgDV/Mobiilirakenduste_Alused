9601d0345c35fa0ec247927b9a14f1f25b3ef853
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2019-10-08 09:49:26 +0300

    Add List_Exercise app.

---
+++ List_Exercise/List_Exercise/First_Example_Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace List_Exercise
+{
+    [Activity(Label = "First_Example_Activity")]
+    public class First_Example_Activity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.First_Example_Layout);
+
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1);
+        }
+    }
+}
