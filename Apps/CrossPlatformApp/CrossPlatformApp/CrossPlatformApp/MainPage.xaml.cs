e887f390bcfb92439505a9e763e503b1af1c57dd
Tehnikumi Opilane <opilane@tptlive.ee>
2020-01-28 08:36:06 +0200

    Add button click.

--- Apps/CrossPlatformApp/CrossPlatformApp/CrossPlatformApp/MainPage.xaml.cs
+++ Apps/CrossPlatformApp/CrossPlatformApp/CrossPlatformApp/MainPage.xaml.cs
@@ -13,5 +13,12 @@ namespace CrossPlatformApp
         {
             InitializeComponent();
         }
+
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You Clicked Me {count}.";
+        }
     }
 }
