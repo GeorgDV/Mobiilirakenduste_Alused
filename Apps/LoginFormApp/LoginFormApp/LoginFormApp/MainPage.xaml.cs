93422a24e84bb0e20ee31aa573fbada5b1fae3bb
Tehnikumi Opilane <opilane@tptlive.ee>
2020-02-04 09:52:05 +0200

    Add LoginFormApp

---
+++ Apps/LoginFormApp/LoginFormApp/LoginFormApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginFormApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignUpBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
