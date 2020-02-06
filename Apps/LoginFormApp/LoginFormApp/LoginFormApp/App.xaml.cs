9c70ff8e1341978ee2976b58d43b29be1add83f5
Tehnikumi Opilane <opilane@tptlive.ee>
2020-02-06 08:36:52 +0200

    Update LoginFormApp

--- Apps/LoginFormApp/LoginFormApp/LoginFormApp/App.xaml.cs
+++ Apps/LoginFormApp/LoginFormApp/LoginFormApp/App.xaml.cs
@@ -7,6 +7,21 @@ namespace LoginFormApp
 {
     public partial class App : Application
     {
+        /*
+        static UserDatabase _dbContext;
+        public static ApplicationDatabase dbContext
+        {
+            get
+            {
+                if (_dbContext == null)
+                {
+                    _dbContext = new ApplicationDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return _dbContext;
+            }
+        }
+        */
+
         public App()
         {
             InitializeComponent();
---
+++ Apps/LoginFormApp/LoginFormApp/LoginFormApp/Data/UserDatabase.cs
@@ -0,0 +1,50 @@
+﻿using LoginFormApp.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformApp.Data
+{
+    public class ApplicationDatabase
+    {
+        SQLiteAsyncConnection _dbContext;
+
+        public ApplicationDatabase(string dbPath)
+        {
+            _dbContext = new SQLiteAsyncConnection(dbPath);
+            _dbContext.CreateTableAsync<User>().Wait();
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            return await _dbContext.Table<User>().ToListAsync();
+        }
+
+        public async Task<User> GetUserAsync(int id)
+        {
+            return await _dbContext.Table<User>()
+                           .Where(x => x.UserId == id)
+                           .FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveUserAsync(User User)
+        {
+            if (User.UserId != 0)
+            {
+                return await _dbContext.UpdateAsync(User);
+            }
+            else
+            {
+                return await _dbContext.InsertAsync(User);
+            }
+        }
+
+        public async Task<int> DeleteUserAsync(User User)
+        {
+            return await _dbContext.DeleteAsync(User);
+        }
+
+    }
+}
